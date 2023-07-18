df3add2205539ea2430cd24ebe866f7bdaa782d8
MatiasRomeo <m.romeov@hotmail.com>
2023-07-18 00:07:14 -0400

    imgs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,14 +19,14 @@ export function Home() {
               alt="First slide"
             />
             <Carousel.Caption>
-              <h3>Raviolisl</h3>
+              <h3>Raviolis</h3>
               <p>Prueba nuestros raviolis caseros, hechos con la mejor harina de fuerza italiana.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="https://img-global.cpcdn.com/recipes/fbadf1dd44fac0a5/680x482cq70/homemade-italian-penne-rigate-pasta-recipe-main-photo.jpg"
+              src="https://images.food52.com/T9IqCqj-126tm6to78WCByJB068=/1008x672/filters:format(webp)/2507db3c-38ab-40d4-afbf-7cae1b9d5fe8--Homemade_Ravioli_with_Ricotta_Cheese_and_Spinach_Filling.jpg"
               alt="Second slide"
             />
 
@@ -38,7 +38,7 @@ export function Home() {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="https://img-global.cpcdn.com/recipes/fbadf1dd44fac0a5/680x482cq70/homemade-italian-penne-rigate-pasta-recipe-main-photo.jpg"
+              src="https://www.gourmet.cl/wp-content/uploads/2016/09/Tortellini-ajustada-web-570x458.jpg"
               alt="Third slide"
             />
 
